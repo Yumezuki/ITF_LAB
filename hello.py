d5fe08d57fd9cab9ad4efc7e6c2d85f282cbf397
Yumezuki <108189514+Yumezuki@users.noreply.github.com>
2023-08-04 15:00:47 +0700

    Update

--- hello.py
+++ hello.py
@@ -1,9 +1,4 @@
-for i in range(10):
-    output = ""
-    if i % 3 == 0:
-        output += "fizz"
-    if i % 5 == 0:
-        output += "buzz"
-    else:
-        output = i
-    print(output)
+# ITF-Lab 2023
+
+## Author
+อัญชิษฐา แก้วรอดฟ้า 66070223
